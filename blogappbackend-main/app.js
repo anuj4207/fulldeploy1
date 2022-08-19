66d6c0dac62cbaf57ec70c01617985360a39626c
Anuj Soni <40125467+anuj4207@users.noreply.github.com>
2022-08-19 20:27:27 +0530

    Update app.js

--- blogappbackend-main/app.js
+++ blogappbackend-main/app.js
@@ -16,6 +16,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(cors())
+spp.get('/',(req,res)=>{console.log("HEllo"),res.json({"h1":"hello"})})
 app.use('/api/v1/blog',blogRoute)
 app.use(notFound)
 app.use(errorHandler)
